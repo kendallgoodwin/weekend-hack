c2b87c934da44a7b11570b1d2b1b2e4201d48226
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-08 14:08:34 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+      $(document).ready(function() {
+      	 $('.parallax').parallax();
+      	
+        $(".fancy_title").lettering();
+        $(".something").lettering('words');
+
+        $(".languages").lettering('words');
+        $(".frameworks").lettering('words');
+        $(".tools").lettering('words');
+        $(".other").lettering('words');
+
+        $(".experience-1").lettering('lines');
+        $(".experience-2").lettering('lines');
+        $(".experience-3").lettering('lines');
+        $(".experience-4").lettering('lines');
+        $(".experience-5").lettering('lines');
+        $(".experience-6").lettering('lines');
+
+      });
+
