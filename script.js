2412708d825f09c573c65a60d34d8332d081834f
kendallgoodwin <kendall@kendallgoodwin.com>
2016-06-12 23:20:18 -0700

    updates

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
       $(document).ready(function() {
-      	 $('.parallax').parallax();
+
+        $('.navbar-fixed').hide();
+        // $('.nav-wrapper').hide();
+        $('.fancy_title').hide();
+        $('.subtitle').hide();
+
+        $('.collapsible').collapsible({
+        accordion : false 
+        // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+
+        $(".button-collapse").sideNav();
+
+      	$('.parallax').parallax();
 
         $(".fancy_title").lettering();
         $(".subtitle").lettering('words');
@@ -21,6 +34,43 @@
         $(".experience-6").lettering('lines');
 
       });
+
+      $.fn.extend({
+        animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+          $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName );
+          });
+        }
+      });
+
+      function nameShow() {
+        $('.fancy_title').animateCss('fadeInUpBig');
+        $('.fancy_title').show();
+      }
+      setTimeout(nameShow, 1500)
+
+      function subtitleShow() {
+        $('.subtitle').animateCss('fadeInUpBig');
+        $('.subtitle').show();
+      }
+      setTimeout(subtitleShow, 2000)
+
+      function navbarShow() {
+        // $('.nav-wrapper').animateCss('fadeIn'); 
+        // $('.nav-wrapper').show();
+        $('.navbar-fixed').show();        
+        $('.navbar-fixed').animateCss('fadeInUpBig');        
+      }
+      setTimeout(navbarShow, 3000);
+
+      function changeToWhite() {
+        $('.parallax-1').css('background-color', '#ffc024');
+      }
+      setTimeout(changeToWhite, 4500)
+
+     
+
 
       // $('h1').mouseenter(function() {
       // 	alert('hi');
